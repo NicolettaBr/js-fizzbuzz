36ec6b8a8d3fbc54ddd89da817297a00ff2acbef
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-03-23 17:01:44 +0100

    if

--- js/script.js
+++ js/script.js
@@ -3,11 +3,18 @@
 //per i multipli di 5 stampi Buzz
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-var listaNumeri = [];
-console.log(listaNumeri);
+var messaggio;
 
 //crea ciclo che contenga numeri da 1 a 100
-for (var i = 0; i < 100; i++);
-console.log(i);
+for (var i = 1; i < 101; i++){
+    console.log(i);
+
+    if(i % 3 == 0 ){
+       messaggio = 'fizz';
+       console.log(messaggio);
+      
+    }
+}
+
 
 
