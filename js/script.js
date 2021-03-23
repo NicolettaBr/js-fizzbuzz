760ae8b724deea9527e2177fbb679b832f8f44dc
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-03-23 17:20:44 +0100

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -3,17 +3,28 @@
 //per i multipli di 5 stampi Buzz
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-var messaggio;
+
 
 //crea ciclo che contenga numeri da 1 a 100
 for (var i = 1; i < 101; i++){
-    console.log(i);
+    
 
-    if(i % 3 == 0 ){
-       messaggio = 'fizz';
-       console.log(messaggio);
+    if(i % 3 == 0 && i % 5 == 0 ){
+    
+       console.log('FizzBuzz');
       
+    }else if(i % 5 == 0 ){
+
+       console.log('Buzz');
+
+    }else if(i % 3 == 0){
+    
+        console.log('Fizz');
+    }else {
+    
+        console.log(i);
     }
+    
 }
 
 
