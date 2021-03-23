d01eb6fcacf17d3a8b05a0f8311aab90ebcf162e
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-03-23 16:22:01 +0100

    inizio esercizio fizzbuzz

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Scrivi un programma che stampi i numeri da 1 a 100
+//per i multipli di 3 stampi “Fizz” al posto del numero
+//per i multipli di 5 stampi Buzz
+//Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+var listaNumeri = [];
+console.log(listaNumeri);
+
+//crea ciclo che contenga numeri da 1 a 100
+for (var i = 0; i < 100; i++);
+console.log(i);
+
+
